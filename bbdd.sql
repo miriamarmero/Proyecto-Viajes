d661e8e75e8a4456f8134c23c8ace00abc2f9a66
Miriam Armero <norma.armero@gmail.com>
2019-05-31 14:48:42 +0200

    Añadido de tabla users a bbdd

--- bbdd.sql
+++ bbdd.sql
@@ -9,3 +9,9 @@ CREATE TABLE destinos(
     fecha_inicio DATE,
     fecha_fin DATE
 );
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(4) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` tinytext NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
